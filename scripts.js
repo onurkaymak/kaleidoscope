16fff87885b5000b2cee7ede008aac006bfb197e
onurkaymak <onurkaymak@outlook.com>
2023-08-09 23:24:57 -0700

    create rgb function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 
+function rgb(min, max) {
+    const r = Math.floor(Math.random() * (max - min) + min).toString();
+    const g = Math.floor(Math.random() * (max - min) + min).toString();
+    const b = Math.floor(Math.random() * (max - min) + min).toString();
+
+    return r.concat(',', g, ',', b);
+}
+
+
+
 
 
 
