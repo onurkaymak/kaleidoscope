ae1817a4de1a76a32e71e51e44b42e06581139c8
onurkaymak <onurkaymak@outlook.com>
2023-08-09 23:26:02 -0700

    create kaleidoscope function to and create function calls

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
+function kaleidoscope(min, max) {
+    function rgb(min, max) {
+        const r = Math.floor(Math.random() * (max - min) + min).toString();
+        const g = Math.floor(Math.random() * (max - min) + min).toString();
+        const b = Math.floor(Math.random() * (max - min) + min).toString();
 
-function rgb(min, max) {
-    const r = Math.floor(Math.random() * (max - min) + min).toString();
-    const g = Math.floor(Math.random() * (max - min) + min).toString();
-    const b = Math.floor(Math.random() * (max - min) + min).toString();
+        return r.concat(',', g, ',', b);
+    }
+    const rgbColor = rgb(min, max);
 
-    return r.concat(',', g, ',', b);
-}
-
-
-
+    return rgbColor;
+};
 
 
 
